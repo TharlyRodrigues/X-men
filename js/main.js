6598e47267c2427c2e69895d939a14472629b5ea
TharlyRodrigues <tharly.portifolio@gmail.com>
2023-07-08 23:24:56 -0300

    update

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const personagens = document.querySelectorAll(".personagem");
+personagens.forEach((personagem) => {
+  personagem.addEventListener("mouseenter", () => {
+    const personagensSelecionados = document.querySelector(".selecionado");
+    personagensSelecionados.classList.remove("selecionado");
+    personagem.classList.add("selecionado");
+
+    const imagemPersonagemGrande = document.querySelector(".personagem-grande");
+
+    const idPersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `imagens-xmen/card-${idPersonagem}.png`;
+
+    const nomePersonagem = document.getElementById("nome-personagem");
+    nomePersonagem.innerText = personagem.getAttribute("data-name");
+  });
+});
